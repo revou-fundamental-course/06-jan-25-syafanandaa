91406db3507010bd7e9a3db9fb703d65be8a732d
syafanandaa <syafafitrananda@gmail.com>
2025-01-16 18:37:31 +0700

    update html css js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,83 @@
 // Ini adalah file javascript
 
-console.log('Hello world!');
+
+// let nameInput = document.getElementById('name-input'); // get input kedalam sebuah variabel 
+
+// document.getElementById('kirim').addEventListener("click", function() { // kemudian apabila di klik tombol kirim maka akan menjalankan sbuah function
+//     const nama = nameInput.value // mendapatkan nama dari inputan
+
+//     if (name == "") {
+//         document.getElementById("error-name").innerHTML = "Nama tidak boleh kosong!";
+//     } else {
+//         document.getElementsById('name').innerHTML = nama; // mengganti nama di html dengan nama yang baru
+//     }
+// }); 
+
+// function validateForm() {
+//     const nama = document.forms['message-form']['name-input'].value; // variabel
+    
+//     if (nama == '') {
+//         document.getElementById("error-name").innerHTML = "Tidak boleh kosong!"
+
+//         return false; // form validationnya berati salah
+//     }
+
+//     // document.getElementById("name").innerHTML = nama;
+//     // document.getElementById("error-name").innerHTML = "";
+
+//     setName(nama)
+
+//     return false;
+// }
+
+// function setName(name) {
+//     document.getElementById("name").innerHTML = name;
+//     document.getElementById("error-name").innerHTML = "";
+
+// }
+
+
 
 function formValidation() {
     // console.log('form validationhas been clicked!');
 
-    let nameInput = document.getElementById('name-input').value;
+    let valid = true;
 
+    let nameInput = document.getElementById('name-input').value;
     console.log(nameInput);
     if (nameInput == ""){
         alert('Name must be filled out');
-    } else {
-        // display result form
-        document.getElementById('result-form').innerHTML = nameInput;
-    }    
+        valid = false;
+    }   
+    
+    let tanggalInput = document.getElementById('tanggal-input').value;
+    console.log(tanggalInput);
+    if (tanggalInput == ""){
+        alert('Tanggal must be filled out');
+        valid = false;
+    }     
+
+    let jkInputL = document.getElementById('jk-input-lakilaki').checked;
+    let jkInputP = document.getElementById('jk-input-perempuan').checked;
+    // console.log(jkInput);
+    if (!jkInputL && !jkInputP){
+        alert('Jenis kelamin must be filled out');
+        valid = false;
+    }   
+    
+    let pesanInput = document.getElementById('pesan-input').value;
+    if (pesanInput == ""){
+        alert('Pesan must be filled out');
+        valid = false;
+    }     
+
+    if  (valid) {
+        document.getElementById('result-form-nama').innerHTML = nameInput;
+        document.getElementById('result-form-tanggal').innerHTML = tanggalInput;
+        // document.getElementById('result-form-jk').innerHTML = jkInput;
+        document.getElementById('result-form-jk').textContent = jkInputL ? "Laki-Laki" : "Perempuan"
+        document.getElementById('result-form-pesan').innerHTML = pesanInput;      
+    }
 }
 
 document.getElementById('submit-btn').addEventListener('click', formValidation);
